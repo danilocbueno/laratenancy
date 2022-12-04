910ba6e74086a5d7beb30abcab011ca787e20fb6
danilocbueno <danilocbueno@gmail.com>
2022-12-04 20:30:06 -0300

    adding tabler

--- resources/views/front/store.blade.php
+++ resources/views/front/store.blade.php
@@ -1,53 +1,89 @@
 <x-guest-layout>
-    <div class="px-6 py-12 md:px-12 bg-gray-100 text-gray-800 text-center lg:text-center">
-        <div class="container mx-auto xl:px-32">
-            <div class="">
-                <div class="mt-12 lg:mt-0">
-                    <h1 class="text-3xl md:text-4xl xl:text-5xl tracking-tight ">você está em a<span class="font-bold  text-blue-600"> {{ $store->name }}</span></h1>
-                    <p class="text-gray-600">
-                        {{ $store->description }}
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
-        <div class="py-2">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden sm:rounded-lg">
-                    <div class="p-6 bg-white border-b border-gray-200 ">
-                        <div class="grid md:grid-cols-4 gap-x-6 gap-y-10">
-                            @forelse($products as $product)
-                                <div class="flex justify-center">
-                                    <div class="rounded-lg bg-white max-w-sm">
-                                        <a href="{{ route('front.single', ['slug' => $product->slug]) }}">
-                                            @if($product->images->count())
-                                                <img src="{{ tenant_asset($product->images->first()->path) }}" class="w-full hover:scale-110 transition duration-300 ease-in-out" alt="..." style="object-fit: cover; max-height: 200px;">
-                                            @else
-                                                <img src="{{ asset('img/no-photo.png') }}" class="w-full hover:scale-110 transition duration-300 ease-in-out" alt="..." style="object-fit: cover; max-height: 200px;">
-                                            @endif
-                                        </a>
-                                        <div class="p-6">
-                                            <h5 class="text-gray-900 text-xl font-medium">{{ $product->name }}</h5>
-                                            <p class="text-gray-700 text-base mb-4">
-                                                {{ $product->description }}
-                                            </p>
-                                            <p class="text-red-700 text-base mb-4">
-                                                R$ {{ $product->price }}
-                                            </p>
-                                            <a href="{{ route('cart.add', ['productSlug' =>  $product->slug]) }}" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Comprar</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            @empty
-                                <p>Ainda não temos produtos cadastrados</p>
-                            @endforelse
+    <div class="page-wrapper">
+        <!-- Page header -->
+        <div class="page-header d-print-none text-white">
+            <div class="container-xl">
+                <div class="row g-2 align-items-center">
+                    <div class="col">
+                        <!-- Page pre-title -->
+                        <div class="page-pretitle">
+                            {{ $store->name }}
+                        </div>
+                        <h2 class="page-title">
+                            {{ $store->name }}
+                        </h2>
+                    </div>
+                    <!-- Page title actions -->
+                    <div class="col-auto ms-auto d-print-none">
+                        <div class="btn-list">
+                            <span class="d-none d-sm-inline">
+                                <a href="#" class="btn btn-dark">
+                                    New view
+                                </a>
+                            </span>
+                            <a href="#" class="btn btn-primary d-none d-sm-inline-block" data-bs-toggle="modal"
+                                data-bs-target="#modal-report">
+                                <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
+                                    viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                                    stroke-linecap="round" stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                    <line x1="12" y1="5" x2="12" y2="19"></line>
+                                    <line x1="5" y1="12" x2="19" y2="12"></line>
+                                </svg>
+                                Create new report
+                            </a>
+                            <a href="#" class="btn btn-primary d-sm-none btn-icon" data-bs-toggle="modal"
+                                data-bs-target="#modal-report" aria-label="Create new report">
+                                <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
+                                    viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                                    stroke-linecap="round" stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                    <line x1="12" y1="5" x2="12" y2="19"></line>
+                                    <line x1="5" y1="12" x2="19" y2="12"></line>
+                                </svg>
+                            </a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        <!-- Page body -->
+        <div class="page-body">
+            <div class="container-xl">
+                <div class="row row-cards d-flex align-items-stretch"">
+                    @forelse($products as $product)
+                        <div class="col-md-6 col-lg-3">
+                            <a href="{{ route('cart.add', ['productSlug' => $product->slug]) }}"
+                                class="card card-link text-center card-link-pop">
+                                <div class="pt-2">
+                                    @if ($product->images->count())
+                                        <img src="{{ tenant_asset($product->images->first()->path) }}" class=""
+                                            alt="..." style="object-fit: cover; max-height: 200px;">
+                                    @else
+                                        <img src="{{ asset('img/no-photo.png') }}" class="" alt="..."
+                                            style="object-fit: cover; max-height: 200px;">
+                                    @endif
+                                </div>
+                                <div class="card-body">
+                                    <h3 class="card-title">{{ $product->name }}</h3>
+                                    <p class="text-muted">{{ $product->description }}</p>
+                                    <div class="display-6 fw-bold my-3">R$ {{ $product->price }}</div>
+                                </div>
+                            </a>
+                        </div>
+                    @empty
+                        <p>Ainda não temos produtos cadastrados</p>
+                    @endforelse
+                </div>
+            </div>
+        </div>
+
     </div>
-    <div class="container mx-auto xl:px-32">
+
+
+    <div class="">
         {{ $products->links() }}
     </div>
 </x-guest-layout>
